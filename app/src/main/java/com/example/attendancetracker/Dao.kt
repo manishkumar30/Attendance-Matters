e8fa8c8b506b14f4804dcac241d5fec4d4292d21
manishkumar30 <manishkumar330442@gmail.com>
2022-01-20 11:59:30 +0530

    Dao

--- app/src/main/java/com/example/attendancetracker/Dao.kt
+++ app/src/main/java/com/example/attendancetracker/Dao.kt
@@ -1,7 +1,17 @@
 package com.example.attendancetracker
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
 import androidx.room.Dao
 
 @Dao
 interface Dao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(item:AttendanceData)
+
+    @Delete
+    suspend fun delete(item:AttendanceData)
+
+    @Query("select * from Attendance")
+    suspend fun getAllData():LiveData<List<AttendanceData>>
 }
